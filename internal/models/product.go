2dcf6ecc852dd895c15db434ba033816fd41093c
Oreva Eniworo <eniworoeva@yahoo.com>
2024-08-27 13:35:15 +0100

    chore: Add Cart and Order models

--- internal/models/product.go
+++ internal/models/product.go
@@ -11,11 +11,7 @@ type Product struct {
 	Quantity    int     `json:"quantity"`
 	Description string  `json:"description"`
 	Status      bool    `json:"status"`
+	Orders      []Order `json:"orders" gorm:"many2many:order_items;"`
 }
 
-type Cart struct {
-	gorm.Model
-	UserID    uint `json:"user_id"`
-	ProductID uint `json:"product_id"`
-	Quantity  int  `json:"quantity"`
-}
+
